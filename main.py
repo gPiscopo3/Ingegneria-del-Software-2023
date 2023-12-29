73222fca9b6ec85922fed137b7f81c9f081e191a
Cristian Vaccarella <vaccarellacristian2@gmail.com>
2023-12-29 16:03:43 +0100

    deleted qualche file di test

--- main.py
+++
@@ -1,8 +0,0 @@
-from APICalls import get_collaborations_since
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    files = get_collaborations_since('2023-11-09T00:00:00Z', 'apache', 'commons-io',
-                                     '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')
-
